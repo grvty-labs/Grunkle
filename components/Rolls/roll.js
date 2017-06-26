0460ea423088be5acea06bd5a59494ef68c98f6c
icecreamsoldier <mariana.9000@hotmail.com>
2017-06-26 11:17:01 -0500

    Post and roll components added

---
+++ components/Rolls/roll.js
@@ -0,0 +1,37 @@
+'use strict';
+import React, { Component } from 'react';
+import Post from './post.js';
+
+class Roll extends Component {
+  render() {
+    const{
+      title,
+      subtitle,
+      posts,
+      change,
+      idNext,
+      idBack,
+      postChange,
+    } = this.props;
+
+    return (
+      <div>
+        <div className = 'header'>
+          <h5 className = 'subtitle'> { subtitle } </h5>
+          <div className = 'title-container'>
+            <h2 className = 'title'> { title }</h2>
+          </div>
+        </div>
+        <Post posts = { posts } postChange={ postChange }/>
+        <div className = 'pagination'>
+          <div className = 'pagination-container'>
+            <p onClick = {() => change(idNext)}>More recent.</p>
+            <p onClick = {() => change(idBack)}>Older.</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Roll;
