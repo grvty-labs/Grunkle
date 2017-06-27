bc51733f32e8de4a220422383f7c1b42d9bae8d7
icecreamsoldier <mariana.9000@hotmail.com>
2017-06-27 11:13:00 -0500

    Render image in Center text component

--- components/CenteredText/CenteredText.jsx
+++ components/CenteredText/CenteredText.jsx
@@ -2,34 +2,42 @@
 import React, { Component } from 'react';
 
 class CenteredText extends Component {
-  render(){
+  renderImage(element) {
+    return (
+      <picture>
+        <source media = '(max-width:768px)' srcSet = { this.props.value.image.thumbs.xs }/>
+        <source media = '(max-width:1024px)' srcSet = { this.props.value.image.thumbs.sm }/>
+        <source media = '(min-width:1024px)' srcSet = { this.props.value.image.thumbs.md }/>
+        <img src = { this.props.value.image.thumbs.original }/>
+      </picture>
+    );
+  }
+
+  render() {
     let background = {
-      backgroundColor: "rgba("+this.props.value.decoration.background_color+")"
-    }
+      backgroundColor: 'rgba(' + this.props.value.decoration.background_color + ')',
+    };
+
     let cta = '-none';
-    if(this.props.value.cta.text != ''){
+    if (this.props.value.cta.text != '') {
       cta = '-show';
     }
-    return(
+
+    return (
       <div className = 'centered-text' style = { background }>
         <div className = 'container'>
           <h4>{ this.props.value.subtitle }</h4>
           <h2>{ this.props.value.title }</h2>
           <p>{ this.props.value.paragraph }</p>
-          <picture>
-            <source media = '(max-width:768px)' srcSet = { this.props.value.image.thumbs.xs }/>
-            <source media = '(max-width:1024px)' srcSet = { this.props.value.image.thumbs.sm }/>
-            <source media = '(min-width:1024px)' srcSet = { this.props.value.image.thumbs.md }/>
-            <img src = { this.props.value.image.thumbs.original }/>
-          </picture>
-          <div className = { 'cta-container'+ cta }>
-            <div className = { this.props.value.cta.breed +cta }>
+          { this.props.value.image != null ? this.renderImage() : null }
+          <div className = { 'cta-container' + cta }>
+            <div className = { this.props.value.cta.breed + cta }>
               { this.props.value.cta.text }
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
