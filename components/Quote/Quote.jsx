5ad1a4ff3c3a03bed3f04044529cb2b1f3b38242
icecreamsoldier <mariana.9000@hotmail.com>
2017-08-03 17:31:00 -0500

    Quote information added

--- components/Quote/Quote.jsx
+++ components/Quote/Quote.jsx
@@ -8,16 +8,20 @@ class Quote extends Component{
         <div className = 'container'>
           <div className = 'image-container'>
             <picture>
-              <source media = '(max-width:1440px)' srcSet = { this.props.value.photograph.thumbs.sm }/>
+              <source media = '(max-width:1440px)' srcSet =
+                { this.props.value.photograph.thumbs.md }/>
               <img src = { this.props.value.photograph.thumbs.original }/>
             </picture>
             <img src = '/static/assets/quote-mark.svg' className = 'quote-mark'/>
           </div>
-          <div className = 'text-container'>
-            <h6>{ this.props.value.quote }</h6>
-            <div className = 'quoter-container'>
-              <h6>{ this.props.value.name }</h6>
-              <p>{ this.props.value.position } at { this.props.value.company }</p>
+          <div className = 'text'>
+            <div className = 'text-container'>
+              <h6>{ this.props.value.quote }</h6>
+              <div className = 'quoter-container'>
+                <h3>{ this.props.value.name }</h3>
+                <p>{ this.props.value.position } at { this.props.value.company }</p>
+              </div>
+              <div className = 'division-rectangle'></div>
             </div>
           </div>
         </div>
